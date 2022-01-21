939970335eb94302e64e9f7f4cdaad72a8402d11
fearhebs1 <fearherbs1@gmail.com>
2022-01-21 14:07:07 -0500

    Homework 2 extra modules

---
+++ Week01/homework/Autiello_syslog_klogind_failed_auth.py
@@ -0,0 +1,26 @@
+from Autiello_syslog_check import _syslog
+
+# klogind failed authentications
+
+
+def klogind_failed(filename, search_terms):
+    # call syslog_check and return the results
+
+    is_found = _syslog(filename, search_terms)
+
+    # found list
+    found = []
+
+    # loop through the results
+    for each_found in is_found:
+        # split the results based on the space dilimeter
+        sp_results = each_found.split(" ")
+
+        # append the split value to the found list
+        found.append(sp_results[4])
+
+    # Remove our duplicates and convert the list to a dict
+    returned_values = set(found)
+    # print our hosts on their own separate line
+    for each_value in returned_values:
+        print(each_value)
